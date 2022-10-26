1b13a359574cd379698d0e79e173c4fc46cfadd7
Rubia Souza <rubia.alice.m.s@gmail.com>
2022-10-26 08:26:39 -0300

    refactor(QuickSort): Ajusta implementação

--- include/QuickSort.h
+++ include/QuickSort.h
@@ -4,94 +4,114 @@
 class QuickSort {
     private:
         QuickSort() {}
-        
+
     public:
         virtual ~QuickSort() {}
 
         template<typename T>
         static void ordenarCrescente(T vetor[], const unsigned int tamanho) {
-            quickSortCrescente(vetor, 0, tamanho - 1);
+            quickSortCrescente(vetor, 0, tamanho-1);
             return;
         }
 
         template<typename T>
         static void ordenarDecrescente(T vetor[], const unsigned int tamanho) {
-            quickSortDecrescente(vetor, 0, tamanho - 1);
+            quickSortDecrescente(vetor, 0, tamanho-1);
             return;
         }
     
     private:
         template<typename T>
-        static void quickSortCrescente(T vetor[], const int posicaoInicial, const int posicaoFinal) {
-            T pivo = vetor[(posicaoInicial + posicaoFinal) / 2];
+        static void quickSortCrescente(T vetor[], int posicaoInicial, int poiscaoFinal) { 
+            int indexEsquerda = 0;
+            int indexDireita = 0;
 
-            int indexElementosEsquerda = posicaoInicial;
-            int indexElementosDireita = posicaoFinal;
+            particionarCrescente(vetor, posicaoInicial, poiscaoFinal, &indexEsquerda, &indexDireita);
 
-            while(indexElementosEsquerda <= indexElementosDireita) {
-                while(vetor[indexElementosEsquerda] < pivo && indexElementosEsquerda < posicaoFinal) {
-                    indexElementosEsquerda++;
-                }
-
-                while(vetor[indexElementosDireita] > pivo && indexElementosDireita > posicaoInicial) {
-                    indexElementosDireita--;
-                }
-
-                if(indexElementosEsquerda <= indexElementosDireita) {
-                    T conteudo = vetor[indexElementosEsquerda];
-                    vetor[indexElementosEsquerda] = vetor[indexElementosDireita];
-                    vetor[indexElementosDireita] = conteudo;
-
-                    indexElementosEsquerda++;
-                    indexElementosDireita--;
-                }
+            if (posicaoInicial < indexDireita) {
+                quickSortCrescente(vetor, posicaoInicial, indexDireita);
             }
 
-            if(indexElementosDireita > posicaoInicial) {
-                quickSortCrescente(vetor, posicaoInicial, indexElementosDireita + 1);
-            }
-
-            if(indexElementosEsquerda < posicaoFinal) {
-                quickSortCrescente(vetor, indexElementosEsquerda, posicaoFinal);
+            if (indexEsquerda < poiscaoFinal) {
+                quickSortCrescente(vetor, indexEsquerda, poiscaoFinal);
             }
 
             return;
         }
 
         template<typename T>
-        static void quickSortDecrescente(T vetor[], const int posicaoInicial, const int posicaoFinal) {
-            T pivo = vetor[(posicaoInicial + posicaoFinal) / 2];
+        static void particionarCrescente(T vetor[], int posicaoInicial, int poiscaoFinal, int* indexEsquerda, int* indexDireita) {
+            *indexEsquerda = posicaoInicial;
+            *indexDireita = poiscaoFinal;
 
-            int indexElementosEsquerda = posicaoInicial;
-            int indexElementosDireita = posicaoFinal;
+            T pivo = vetor[(*indexEsquerda + *indexDireita) / 2];
 
-            while(indexElementosEsquerda <= indexElementosDireita) {
-                while(vetor[indexElementosEsquerda] > pivo && indexElementosEsquerda < posicaoFinal) {
-                    indexElementosEsquerda++;
+            do {
+                while (pivo > vetor[*indexEsquerda]) {
+                    (*indexEsquerda)++;
                 }
 
-                while(vetor[indexElementosDireita] < pivo && indexElementosDireita > posicaoInicial) {
-                    indexElementosDireita--;
+                while (pivo < vetor[*indexDireita]) {
+                    (*indexDireita)--;
                 }
 
-                if(indexElementosEsquerda <= indexElementosDireita) {
-                    T conteudo = vetor[indexElementosEsquerda];
-                    vetor[indexElementosEsquerda] = vetor[indexElementosDireita];
-                    vetor[indexElementosDireita] = conteudo;
+                if (*indexEsquerda <= *indexDireita) {
+                    T trocaTemporaria = vetor[*indexEsquerda];
+                    vetor[*indexEsquerda] = vetor[*indexDireita];
+                    vetor[*indexDireita] = trocaTemporaria;
 
-                    indexElementosEsquerda++;
-                    indexElementosDireita--;
+                    (*indexEsquerda)++;
+                    (*indexDireita)--;
                 }
+            } while(*indexEsquerda <= *indexDireita);
+            
+            return;
+        }
+
+        template<typename T>
+        static void quickSortDecrescente(T vetor[], int posicaoInicial, int poiscaoFinal) { 
+            int indexEsquerda = 0;
+            int indexDireita = 0;
+
+            particionarDecrescente(vetor, posicaoInicial, poiscaoFinal, &indexEsquerda, &indexDireita);
+
+            if (posicaoInicial < indexDireita) {
+                quickSortDecrescente(vetor, posicaoInicial, indexDireita);
             }
 
-            if(indexElementosDireita > posicaoInicial) {
-                quickSortDecrescente(vetor, posicaoInicial, indexElementosDireita + 1);
+            if (indexEsquerda < poiscaoFinal) {
+                quickSortDecrescente(vetor, indexEsquerda, poiscaoFinal);
             }
 
-            if(indexElementosEsquerda < posicaoFinal) {
-                quickSortDecrescente(vetor, indexElementosEsquerda, posicaoFinal);
-            }
+            return;
+        }
 
+        template<typename T>
+        static void particionarDecrescente(T vetor[], int posicaoInicial, int poiscaoFinal, int* indexEsquerda, int* indexDireita) {
+            *indexEsquerda = posicaoInicial;
+            *indexDireita = poiscaoFinal;
+
+            T pivo = vetor[(*indexEsquerda + *indexDireita) / 2];
+
+            do {
+                while (pivo < vetor[*indexEsquerda]) {
+                    (*indexEsquerda)++;
+                }
+
+                while (pivo > vetor[*indexDireita]) {
+                    (*indexDireita)--;
+                }
+
+                if (*indexEsquerda <= *indexDireita) {
+                    T trocaTemporaria = vetor[*indexEsquerda];
+                    vetor[*indexEsquerda] = vetor[*indexDireita];
+                    vetor[*indexDireita] = trocaTemporaria;
+
+                    (*indexEsquerda)++;
+                    (*indexDireita)--;
+                }
+            } while(*indexEsquerda <= *indexDireita);
+            
             return;
         }
 };
